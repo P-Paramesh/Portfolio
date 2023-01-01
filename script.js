ea2468ab43c7665bcd2309f8849cdb581d74d07d
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-01-01 21:30:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+class Board extends React.Component {
+  constructor(props) {    super(props);    this.state = {      squares: Array(9).fill(null),    };  }
+  renderSquare(i) {
+    return <Square value={i} />;
+  }
