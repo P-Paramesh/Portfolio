3e4d3564a473b34d64f0ae2a076b384f2b6c4d0d
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-08-17 18:36:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
+import "./index.css";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile"; 
+import Projects from "./routes/Projects";
+import Contact from "./routes/Contact";
+import { Route, Routes } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div>
-      <h1>
-        Hello world
-      </h1>
-    </div>
+   <>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/profile" element={<Profile />}/>
+      <Route path="/project" element={<Projects />}/>
+      <Route path="/contact" element={<Contact />}/>     
+    </Routes>
+   </>
   )
 }
 export default App;
