0bdf1d2025198d00e569dc6fb96098469c1de980
P.Paramesh <95079545+P-Paramesh@users.noreply.github.com>
2023-10-04 12:15:52 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 
 import React from 'react'
+import { Link } from "react-router-dom"
+import "./NavbarStyles.css"
 
 const Navbar = () => {
 return (
-    <div>Navbar
-
-    </div>  )
+    <div className="header">
+        <Link to="/">
+            <h1>Portfolio</h1>
+        </Link>
+        <ul>
+            <li>
+                <Link to="/">Home</Link>
+                <Link to="/profile">Profile</Link>
+                <Link to="/project">Projects</Link>
+                <Link to="/contact">Contact</Link>
+            </li>
+        </ul>
+    </div>  
+    )
 }
 
 export default Navbar
